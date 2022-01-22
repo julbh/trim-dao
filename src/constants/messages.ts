ee93e247bcb56aea74e968e46723f2aad50c0111
qualifyDev <ellenabarasova@gmail.com>
2022-01-22 12:39:18 +0900

    first commit

---
+++ src/constants/messages.ts
@@ -0,0 +1,15 @@
+export const messages = {
+    please_connect: "Please connect your wallet to the Avalanche network to use TRIM Public Sale.",
+    please_connect_wallet: "Please connect your wallet.",
+    tx_successfully_send: "Your transaction was successfully sent",
+    your_balance_updated: "Your balance was successfully updated",
+    nothing_to_claim: "You have nothing to claim",
+    something_wrong: "Something went wrong",
+    switch_to_avalanche: "Switch to the Avalanche network?",
+    your_balance_update_soon: "Your balance will update soon",
+    before_buyToken: "Amount is not correct. Please contact with admin first.",
+    before_purchase: "Before purchasing, enter a value.",
+    before_claim: "Before claiming, enter a value.",
+    claim_start: "You can claim TRIM after the publicsale ends.",
+    claimable_zero: "Your claimable TRIM is not exist now.",
+};
